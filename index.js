ff71080662f3a042c9ccc84e37a65d514ef8ba5f
Jake <505e06b2@gmail.com>
2020-07-04 22:29:23 +0100

    Altered settings system

--- index.js
+++ index.js
@@ -6,77 +6,10 @@ const fs = require("fs");
 const path = require("path");
 const string_decoder = require('string_decoder').StringDecoder;
 
-const styles_dir = "styles";
-const scripts_dir = "scripts";
-const separator = " ";
-
-class Settings {
-	constructor(file_path) {
-		this.file_path = file_path;
-		let default_settings = "800\n600\n0\n0\ntrue\ntrue";
-
-		// Write default settings if the settings file does not exist.
-		if (!fs.existsSync(this.file_path)) {
-			fs.writeFileSync(this.file_path, default_settings, (err) => { if (err) throw err; });
-		}
-
-		// Read settings file and assign attributes based on the split.
-		let data = fs.readFileSync(this.file_path, 'utf8').split("\n");
-		this.width = Number(data[0]);
-		this.height = Number(data[1]);
-		this.x = Number(data[2]);
-		this.y = Number(data[3]);
-		if(data[4] === "true") {
-			this.close_to_taskbar = true;
-		} else {
-			this.close_to_taskbar = false;
-		}
-		if(data[5] === "true") {
-			this.no_menu = true;
-		} else {
-			this.no_menu = false;
-		}
-	}
-
-	// Sets the offset of the class.
-	// This is necessary to keep the same window position over time.
-	// Needs to be called after the window has been drawn.
-	setOffset(window_position) {
-		this.offset_x = window_position[0] - this.x;
-		this.offset_y = window_position[1] - this.y;
-	}
-
-	// Updates the x and y attributes to the current window position accounting for the offset.
-	updatePosition(window_position) {
-		this.x = window_position[0] - this.offset_x;
-		this.y = window_position[1] - this.offset_y;
-	}
-
-	// Save the current settings to file.
-	save() {
-		let data = `${this.width}\n${this.height}\n${this.x}\n${this.y}\n${this.close_to_taskbar}\n${this.no_menu}`;
-		fs.writeFileSync(this.file_path, data, (err) => { if (err) throw err; });
-	}
-}
+const separator = " "; //for native UI
 
 electron.app.on("ready", () => {
 	let quit = false;
-	let settings = new Settings("settings.txt")
-
-	const win = new electron.BrowserWindow({
-		width: settings.width,
-		height: settings.height,
-		webPreferences: {
-			nodeIntegration: false,
-			spellcheck: true
-		},
-		titleBarStyle: "hidden",
-		icon: "assets/icon.png"
-	});
-
-	win.setPosition(settings.x, settings.y);
-	if(settings.no_menu) { win.removeMenu(); }
-	//win.webContents.openDevTools();
 
 	const tray_icon = new electron.Tray("assets/icon.png");
 	const context_menu = electron.Menu.buildFromTemplate([
@@ -91,39 +24,107 @@ electron.app.on("ready", () => {
 			}
 		},
 	]);
+	tray_icon.setToolTip("OpenDiscord");
+	tray_icon.on("click", (event, bounds, position) => win.show());
 	tray_icon.setContextMenu(context_menu);
 
-	win.on("close", (event) => {
-		if(settings.close_to_taskbar) {
-			if(!quit) {
-				event.preventDefault();
-				win.hide();
-				return false;
+	const win = new electron.BrowserWindow({
+		webPreferences: {
+			nodeIntegration: false,
+			spellcheck: true
+		},
+
+		show: false,
+		width: 800,
+		height: 400,
+		minWidth: 100,
+		minHeight: 100,
+
+		icon: "assets/icon.png"
+	});
+
+	//win.setPosition(settings.x, settings.y);
+	//if(settings.no_menu) { win.removeMenu(); }
+	//win.webContents.openDevTools();
+
+	const settings = new (function () {
+		const settings_file = "settings.json";
+		const defaults = {
+			styles_dir: "styles",
+			scripts_dir: "scripts",
+			show_menu_bar: true,
+			window_bounds: {},
+			close_to_tray: true,
+			start_in_tray: false
+		};
+		fs.writeFileSync("default_settings.json", JSON.stringify(defaults, null, 4));
+		let contents = defaults;
+
+		this.load = () => {
+			try {
+				const file_contents = JSON.parse(fs.readFileSync(settings_file));
+				if(typeof(file_contents) !== "object" || Array.isArray(file_contents)) throw "Settings is not an object";
+				contents = file_contents;
+				console.log(`Loaded ${settings_file}`);
+			} catch(e) {
+				console.error(`Cannot load ${settings_file}:\n${e}`);
 			}
+		};
+
+		this.save = () => {
+			contents.window_bounds = win.getBounds();
+			fs.writeFileSync(settings_file, JSON.stringify(contents, null, 4));
 		}
-		settings.updatePosition(win.getPosition());
+
+		this.get = (key) => { //fall back to defaults
+			if(typeof(contents[key]) !== "undefined") return contents[key];
+			return defaults[key];
+		};
+	})();
+
+
+	// == Window Events ==
+	win.on("close", (event) => {
+		if(!quit) {
+			event.preventDefault();
+			win.hide();
+			return false;
+		}
 		settings.save();
 	});
 
-	win.webContents.on("dom-ready", async (event) => {
-		settings.setOffset(win.getPosition());
-
-		const readFile = (folder, name) => {
+	// == Document Events ==
+	win.webContents.on("dom-ready", (event) => {
+		console.log("\n========== PAGE LOAD ==========\n");
+		function readFile(folder, name) {
 			return (new string_decoder()).write(fs.readFileSync(path.join(folder, name)));
-		};
-
-		console.log("\nDOM Ready...");
-		console.log(`Loaded CSS files (${styles_dir}/):`);
-		for(const file_name of fs.readdirSync(styles_dir)) {
-			win.webContents.insertCSS( readFile(styles_dir, file_name) );
-			console.log(`- ${file_name}`);
 		}
 
-		console.log(`\nLoaded Javascript files (${scripts_dir}/):`);
-		for(const file_name of fs.readdirSync(scripts_dir)) {
-			win.webContents.executeJavaScript(readFile(scripts_dir, file_name));
-			console.log(`- ${file_name}`);
-		}
+		settings.load();
+		try {
+			win.setBounds(settings.get("window_bounds"));
+		} catch {console.error("window_bounds was not valid");}
+		quit = !settings.get("close_to_tray");
+		if(!settings.get("start_in_tray")) win.show();
+		win.setMenuBarVisibility( settings.get("show_menu_bar") );
+
+		const styles_dir = settings.get("styles_dir");
+		const scripts_dir = settings.get("scripts_dir");
+
+		//Load mods
+		(async () => {
+			console.log(`\nLoaded CSS files (${styles_dir}/):`);
+			for(const file_name of fs.readdirSync(styles_dir)) {
+				win.webContents.insertCSS( readFile(styles_dir, file_name) );
+				console.log(`- ${file_name}`);
+			}
+
+			console.log(`\nLoaded Javascript files (${scripts_dir}/):`);
+			for(const file_name of fs.readdirSync(scripts_dir)) {
+				win.webContents.executeJavaScript(readFile(scripts_dir, file_name));
+				console.log(`- ${file_name}`);
+			}
+		})();
 	});
 
 	//document.querySelector('link[rel="icon"]').href
@@ -170,18 +171,6 @@ electron.app.on("ready", () => {
 			menu.popup();
 		}
 	});
-
-	/*
-	//Disable CSP, so that inline JS can be injected
-	electron.session.defaultSession.webRequest.onHeadersReceived((details, callback) => {
-		for(const key in details.responseHeaders) {
-			if(key === "content-security-policy") {
-				details.responseHeaders[key] = [ "" ];
-			}
-		}
-		callback(details);
-	});
-	*/
 
 	win.loadURL("https://discord.com/channels/@me/");
 });
