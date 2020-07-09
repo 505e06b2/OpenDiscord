31803347fffab510a6f5fc848b4b2c5e67c2edc0
Jake <505e06b2@gmail.com>
2020-07-09 21:48:50 +0100

    Clean up killer

--- scripts/killer.js
+++ scripts/killer.js
@@ -5,16 +5,10 @@ const killer = new (function() {
 	const SERVER = "199993370691633152"
 	const CHANNELS = ["730886083989995561"];
 
-	//Retard Zone
-	//const SERVER = "540586684782477313";
-	//const CHANNELS = ["723125510284247040", "723978747971240067"];
-
-	const USERAGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.106 Safari/537.36";
 	const MESSAGE = "https://discord.com/invite/sbABaJy https://discord.com/invite/j8YAqBe https://discord.com/invite/3cKVsvz https://discord.com/invite/953arjf https://discord.com/invite/uZwWJkQ";
 
 	function generateSnowflake() {
 		const epoch = (new Date("2015-01-01")).getTime();
-
 		let ret = BigInt(0);
 		ret = ret + (BigInt((new Date()).getTime() - epoch) << 22n);
 		ret = ret + (BigInt(1) << 17n);
@@ -25,23 +19,23 @@ const killer = new (function() {
 
 	async function messageTemplate(channel, message, referrer) {
 		return await fetch(`https://discord.com/api/v6/channels/${channel}/messages`, {
-		  "headers": {
-			"accept": "*/*",
-			"accept-language": "en-GB",
-			"authorization": "",
-			"cache-control": "no-cache",
-			"content-type": "application/json",
-			"user-agent": USERAGENT,
-			"pragma": "no-cache",
-			"sec-fetch-dest": "empty",
-			"sec-fetch-mode": "cors",
-			"sec-fetch-site": "same-origin"
-		  },
-		  "referrer": referrer,
-		  "referrerPolicy": "no-referrer-when-downgrade",
-		  "body": `{\"content\":\"${message}\",\"nonce\":\"${generateSnowflake()}\",\"tts\":false}`,
-		  "method": "POST",
-		  "mode": "cors"
+			"headers": {
+				"accept": "*/*",
+				"accept-language": "en-GB",
+				"authorization": "",
+				"cache-control": "no-cache",
+				"content-type": "application/json",
+				"pragma": "no-cache",
+				"sec-fetch-dest": "empty",
+				"sec-fetch-mode": "cors",
+				"sec-fetch-site": "same-origin"
+			},
+			"referrer": referrer,
+			"referrerPolicy": "no-referrer-when-downgrade",
+			"body": `{\"content\":\"${message}\",\"nonce\":\"${generateSnowflake()}\",\"tts\":false}`,
+			"method": "POST",
+			"mode": "cors",
+			"credentials": "include"
 		});
 	}
 
